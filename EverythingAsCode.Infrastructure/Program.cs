c92e3410470252199051596993a03357e184f20c
Andy Stewart <Andrew.Stewart@i-nnovate.net>
2024-02-14 21:10:11 +0000

    Remove storage account

--- EverythingAsCode.Infrastructure/Program.cs
+++ EverythingAsCode.Infrastructure/Program.cs
@@ -9,21 +9,10 @@ return await Pulumi.Deployment.RunAsync(() =>
     // Create an Azure Resource Group
     var resourceGroup = new Environment();
 
-    // Create an Azure resource (Storage Account)
-    var storageAccount = new StorageAccount("sa", new StorageAccountArgs
-    {
-        ResourceGroupName = resourceGroup.ResourceGroup.Name,
-        Sku = new SkuArgs
-        {
-            Name = SkuName.Standard_LRS
-        },
-        Kind = Kind.StorageV2
-    });
-
     // Export the primary key of the Storage Account
     return new Dictionary<string, object?>
     {
-        ["primaryStorageKey"] = ""
+        ["primaryStorageKey"] = primaryStorageKey
     };
 });
 
