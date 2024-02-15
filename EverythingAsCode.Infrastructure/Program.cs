24c4e8c575fa5ac48067aecb493496e734a4fd99
Andrew Stewart <andrew.stewart@i-nnovate.net>
2024-02-15 20:48:50 +0000

    Create a container environment

--- EverythingAsCode.Infrastructure/Program.cs
+++ EverythingAsCode.Infrastructure/Program.cs
@@ -1,27 +1,31 @@
-﻿using Pulumi;
+﻿using System.Collections.Generic;
 using Pulumi.AzureNative.Resources;
-using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
-using System.Collections.Generic;
+using Pulumi.AzureNative.Web.V20230101;
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
     // Create an Azure Resource Group
     var resourceGroup = new Environment();
+    var kubeEnv = new KubeEnvironment(
+        "env",
+        new KubeEnvironmentArgs
+        {
+            ResourceGroupName = resourceGroup.ResourceGroup.Name,
+            EnvironmentType = "Managed"
+        }
+    );
 
     // Export the primary key of the Storage Account
-    return new Dictionary<string, object?>
-    {
-        ["primaryStorageKey"] = ""
-    };
+    return new Dictionary<string, object?> { ["primaryStorageKey"] = "" };
 });
 
-public class Environment {
-
+public class Environment
+{
     public ResourceGroup ResourceGroup { get; }
-    
+
     public Environment()
     {
         ResourceGroup = new ResourceGroup("resourceGroup");
     }
 }
+
