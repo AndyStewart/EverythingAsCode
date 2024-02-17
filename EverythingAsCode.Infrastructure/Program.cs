e2ab7917ca22fcb992fb84c76bd95655ebec61e8
Andy Stewart <Andrew.Stewart@i-nnovate.net>
2024-02-17 16:58:10 +0000

    hard code values

--- EverythingAsCode.Infrastructure/Program.cs
+++ EverythingAsCode.Infrastructure/Program.cs
@@ -38,9 +38,9 @@ return await Pulumi.Deployment.RunAsync(async () =>
         "ACRPullRole",
         new RoleAssignmentArgs
         {
-            PrincipalId = identity.PrincipalId.Apply(id => id),
+            PrincipalId = "/subscriptions/466a09cb-2d6e-4824-9190-47a90985f8b6/resourcegroups/rg-everyascodec187ad2d/providers/Microsoft.ManagedIdentity/userAssignedIdentities/identityc6c2f6b9",
             RoleDefinitionId = "/subscriptions/466a09cb-2d6e-4824-9190-47a90985f8b6/providers/Microsoft.Authorization/roleDefinitions/7f951dda-4ed3-4680-a7ca-43fe172d538d",
-            Scope = "/subscriptions/466a09cb-2d6e-4824-9190-47a90985f8b6/resourceGroups/rg-andy-infrastructure/providers/Microsoft.ContainerRegistry/registries/andystewartregistry"
+            Scope = "/subscriptions/466a09cb-2d6e-4824-9190-47a90985f8b6"
         },
         new CustomResourceOptions { Parent = containerEnv }
     );
