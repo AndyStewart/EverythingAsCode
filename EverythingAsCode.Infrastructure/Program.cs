55f10410130b97873275d3ec1a4b48af0452628c
Andy Stewart <Andrew.Stewart@i-nnovate.net>
2024-02-17 14:21:13 +0000

    Deploy a container

--- EverythingAsCode.Infrastructure/Program.cs
+++ EverythingAsCode.Infrastructure/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using EverythingAsCode.Infrastructure;
 using Pulumi.AzureNative.App;
+using Pulumi.AzureNative.App.Inputs;
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
@@ -9,6 +10,26 @@ return await Pulumi.Deployment.RunAsync(() =>
     var containerEnv = new ManagedEnvironment(
         "env",
         new ManagedEnvironmentArgs { ResourceGroupName = environment.ResourceGroup.Name }
+    );
+
+    // Add a azure container app to deploy a docker container called andy/geoff:latest into the containerEnv
+    var containerApp = new ContainerApp(
+        "containerApp",
+        new ContainerAppArgs
+        {
+            ResourceGroupName = environment.ResourceGroup.Name,
+            Location = environment.ResourceGroup.Location,
+            EnvironmentId = containerEnv.Id,
+            Template = new TemplateArgs
+            {
+                Containers = new ContainerArgs[] {
+                    new ContainerArgs {
+                        Name = "andygeoff",
+                        Image = "andy/geoff:latest",
+                    }
+                }
+            }
+        }
     );
 
     // Export the primary key of the Storage Account
