adf584560b6698c45d48a37ac98c4a172a537f22
Andy Stewart <Andrew.Stewart@i-nnovate.net>
2024-02-17 16:37:51 +0000

    remove app creation

--- EverythingAsCode.Infrastructure/Program.cs
+++ EverythingAsCode.Infrastructure/Program.cs
@@ -1,19 +1,17 @@
 ﻿using System.Collections.Generic;
 using EverythingAsCode.Infrastructure;
-using Microsoft.Win32;
 using Pulumi;
 using Pulumi.AzureNative.App;
 using Pulumi.AzureNative.App.Inputs;
-using Pulumi.AzureNative.Authorization;
 using Pulumi.AzureNative.ContainerRegistry;
-using Pulumi.AzureNative.DBforPostgreSQL;
+using Pulumi.AzureNative.Authorization;
 using Pulumi.AzureNative.ManagedIdentity;
 using ManagedServiceIdentityType = Pulumi.AzureNative.App.ManagedServiceIdentityType;
 
 return await Pulumi.Deployment.RunAsync(async () =>
 {
-    // Create an Azure Resource Group
     var environment = new Environment();
+
     var containerEnv = new ManagedEnvironment(
         "env",
         new ManagedEnvironmentArgs { ResourceGroupName = environment.ResourceGroup.Name }
@@ -41,29 +39,29 @@ return await Pulumi.Deployment.RunAsync(async () =>
         new CustomResourceOptions { Parent = containerEnv }
     );
 
-    var containerApp = new ContainerApp(
-        "cap-everythingascode",
-        new ContainerAppArgs
-        {
-            ResourceGroupName = environment.ResourceGroup.Name,
-            Location = environment.ResourceGroup.Location,
-            EnvironmentId = containerEnv.Id,
-            Identity = new ManagedServiceIdentityArgs
-            {
-                Type = ManagedServiceIdentityType.UserAssigned,
-                UserAssignedIdentities = { identity.Id },
-            },
-            Template = new TemplateArgs
-            {
-                Containers = new ContainerArgs[] {
-                    new ContainerArgs {
-                        Name = "everythingascode",
-                        Image = "andystewartregistry.azurecr.io/my-app:1.0",
-                    }
-                }
-            }
-        }
-    );
+    // var containerApp = new ContainerApp(
+    //     "cap-everythingascode",
+    //     new ContainerAppArgs
+    //     {
+    //         ResourceGroupName = environment.ResourceGroup.Name,
+    //         Location = environment.ResourceGroup.Location,
+    //         EnvironmentId = containerEnv.Id,
+    //         Identity = new ManagedServiceIdentityArgs
+    //         {
+    //             Type = ManagedServiceIdentityType.UserAssigned,
+    //             UserAssignedIdentities = { identity.Id },
+    //         },
+    //         Template = new TemplateArgs
+    //         {
+    //             Containers = new ContainerArgs[] {
+    //                 new ContainerArgs {
+    //                     Name = "everythingascode",
+    //                     Image = "andystewartregistry.azurecr.io/my-app:1.0",
+    //                 }
+    //             }
+    //         }
+    //     }
+    // );
 
     // Export the primary key of the Storage Account
     return new Dictionary<string, object?> { ["primaryStorageKey"] = "" };
