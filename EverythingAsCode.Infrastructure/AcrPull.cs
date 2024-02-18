3fc46ef82e309af7b7eef73d24df557a2bef9687
Andrew Stewart <andrew.stewart@i-nnovate.net>
2024-02-18 20:59:32 +0000

    Added AcrPullabstraction

--- EverythingAsCode.Infrastructure/AcrPull.cs
+++ EverythingAsCode.Infrastructure/AcrPull.cs
@@ -8,5 +8,19 @@ internal class AcrPull
 {
     private readonly RoleAssignment acrRole;
 
-    public AcrPull(UserAssignedIdentity identity, GetRegistryResult containerEnv) { }
+    public AcrPull(UserAssignedIdentity identity, GetRegistryResult containerEnv)
+    {
+        acrRole = new RoleAssignment(
+            "AcrRoleAssignment",
+            new RoleAssignmentArgs
+            {
+                PrincipalId = identity.PrincipalId,
+                PrincipalType = "ServicePrincipal",
+                RoleDefinitionId =
+                    "/subscriptions/466a09cb-2d6e-4824-9190-47a90985f8b6/providers/Microsoft.Authorization/roleDefinitions/7f951dda-4ed3-4680-a7ca-43fe172d538d",
+                Scope = containerEnv.Id
+            },
+            new() { DeleteBeforeReplace = true }
+        );
+    }
 }
