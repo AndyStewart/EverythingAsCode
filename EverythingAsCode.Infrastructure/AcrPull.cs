6718cbac9d23015524b6ef257c50b7efc34309bd
Andrew Stewart <andrew.stewart@i-nnovate.net>
2024-02-18 20:35:30 +0000

    Added AcrPullabstraction

---
+++ EverythingAsCode.Infrastructure/AcrPull.cs
@@ -0,0 +1,25 @@
+using Pulumi.AzureNative.Authorization;
+using Pulumi.AzureNative.ContainerRegistry;
+using Pulumi.AzureNative.ManagedIdentity;
+
+namespace EverythingAsCode.Infrastructure;
+
+internal class AcrPull
+{
+    private RoleAssignment acrRole;
+
+    public AcrPull(UserAssignedIdentity identity, GetRegistryResult containerEnv)
+    {
+        acrRole = new RoleAssignment(
+            "acrRole",
+            new RoleAssignmentArgs
+            {
+                PrincipalId = identity.PrincipalId,
+                PrincipalType = "ServicePrincipal",
+                RoleDefinitionId =
+                    "/subscriptions/466a09cb-2d6e-4824-9190-47a90985f8b6/providers/Microsoft.Authorization/roleDefinitions/7f951dda-4ed3-4680-a7ca-43fe172d538d",
+                Scope = containerEnv.Id
+            }
+        );
+    }
+}
