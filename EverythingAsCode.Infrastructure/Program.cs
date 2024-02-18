0819daa8e20e775573f223fea906c6a752d2d494
Andrew Stewart <andrew.stewart@i-nnovate.net>
2024-02-18 19:55:56 +0000

    Added editor config

--- EverythingAsCode.Infrastructure/Program.cs
+++ EverythingAsCode.Infrastructure/Program.cs
@@ -3,25 +3,26 @@ using EverythingAsCode.Infrastructure;
 using Pulumi;
 using Pulumi.AzureNative.App;
 using Pulumi.AzureNative.App.Inputs;
-using Pulumi.AzureNative.ContainerRegistry;
 using Pulumi.AzureNative.Authorization;
+using Pulumi.AzureNative.ContainerRegistry;
 using Pulumi.AzureNative.ManagedIdentity;
 using ManagedServiceIdentityType = Pulumi.AzureNative.App.ManagedServiceIdentityType;
 
-return await Pulumi.Deployment.RunAsync(async () =>
+return await Deployment.RunAsync(async () =>
 {
     var environment = new Environment();
-
     var containerEnv = new ManagedEnvironment(
         "env",
         new ManagedEnvironmentArgs { ResourceGroupName = environment.ResourceGroup.Name }
     );
 
-    var containerRegistry = await GetRegistry.InvokeAsync(new GetRegistryArgs
-    {
-        RegistryName = "andystewartregistry",
-        ResourceGroupName = "rg-andy-infrastructure"
-    });
+    var containerRegistry = await GetRegistry.InvokeAsync(
+        new GetRegistryArgs
+        {
+            RegistryName = "andystewartregistry",
+            ResourceGroupName = "rg-andy-infrastructure"
+        }
+    );
 
     var identity = new UserAssignedIdentity(
         "identity",
@@ -34,12 +35,14 @@ return await Pulumi.Deployment.RunAsync(async () =>
         {
             PrincipalId = identity.PrincipalId,
             PrincipalType = "ServicePrincipal",
-            RoleDefinitionId = "/subscriptions/466a09cb-2d6e-4824-9190-47a90985f8b6/providers/Microsoft.Authorization/roleDefinitions/7f951dda-4ed3-4680-a7ca-43fe172d538d",
+            RoleDefinitionId =
+                "/subscriptions/466a09cb-2d6e-4824-9190-47a90985f8b6/providers/Microsoft.Authorization/roleDefinitions/7f951dda-4ed3-4680-a7ca-43fe172d538d",
             Scope = containerRegistry.Id
         },
         new CustomResourceOptions { Parent = containerEnv }
     );
 
+    var imageName = "andystewartregistry.azurecr.io/my-app:1.0";
     var containerApp = new ContainerApp(
         "cap-everythingascode",
         new ContainerAppArgs
@@ -71,11 +74,9 @@ return await Pulumi.Deployment.RunAsync(async () =>
             },
             Template = new TemplateArgs
             {
-                Containers = new ContainerArgs[] {
-                    new ContainerArgs {
-                        Name = "everythingascode",
-                        Image = "andystewartregistry.azurecr.io/my-app:1.0",
-                    }
+                Containers = new ContainerArgs[]
+                {
+                    new ContainerArgs { Name = "everythingascode", Image = imageName }
                 },
             },
         }
