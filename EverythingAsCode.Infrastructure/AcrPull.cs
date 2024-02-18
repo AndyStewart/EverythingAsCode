3de7dafd58e07b87b87c4e703ed1028700fbc8c7
Andrew Stewart <andrew.stewart@i-nnovate.net>
2024-02-18 20:40:25 +0000

    Added AcrPullabstraction

--- EverythingAsCode.Infrastructure/AcrPull.cs
+++ EverythingAsCode.Infrastructure/AcrPull.cs
@@ -19,7 +19,8 @@ internal class AcrPull
                 RoleDefinitionId =
                     "/subscriptions/466a09cb-2d6e-4824-9190-47a90985f8b6/providers/Microsoft.Authorization/roleDefinitions/7f951dda-4ed3-4680-a7ca-43fe172d538d",
                 Scope = containerEnv.Id
-            }
+            },
+            new() { DeleteBeforeReplace = true }
         );
     }
 }
