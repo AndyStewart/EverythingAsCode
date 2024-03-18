6b208fe0d319e217525574788c740d4b02de40ce
Andrew Stewart <andrew.stewart@i-nnovate.net>
2024-03-18 19:44:09 +0000

    Added tests

---
+++ EverythingAsCode.Api.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace EverythingAsCode.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+            Assert.True(false);
+        }
+    }
+}
