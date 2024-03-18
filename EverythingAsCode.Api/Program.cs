2a44da0a9263f5632dff0448cc4ee90a74bf69dd
Andrew Stewart <andrew.stewart@i-nnovate.net>
2024-03-18 21:03:10 +0000

    Basic event publisher, listener and event outputter

--- EverythingAsCode.Api/Program.cs
+++ EverythingAsCode.Api/Program.cs
@@ -1,5 +1,6 @@
 using Azure;
 using Azure.Messaging.EventGrid;
+using Microsoft.Extensions.Azure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -46,15 +47,22 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
-
+var client = new EventGridPublisherClient(
+    new Uri("https://everythingascodetopic.westeurope-1.eventgrid.azure.net/api/events"),
+    new AzureKeyCredential(configuration["EVENTGRID_TOPIC_KEY"]));
 app.MapPost("/publish", async () =>
 {
-    var client = new EventGridPublisherClient(
-        new Uri("https://everythingascodetopic.westeurope-1.eventgrid.azure.net/api/events"),
-        new AzureKeyCredential(configuration["EVENTGRID_TOPIC_KEY"]));
     var eventGridEvent = new EventGridEvent("ThingHappened", "Thing", "ThingData", new { A = 1});
     await client.SendEventAsync(eventGridEvent);
 });
+var events = new List<string>();
+app.MapPost("/listen", async context =>
+{
+    var rawRequestBody = new StreamReader(context.Request.Body).ReadToEnd();
+    events.Add(rawRequestBody);
+});
+app.MapGet("/events", () => events.OrderDescending().ToList());
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
